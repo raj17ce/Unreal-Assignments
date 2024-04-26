202e1ef77999dada05ce684ba973b7197e0eb143
Raj Patel <raj17ce@gmail.com>
2024-04-26 12:37:02 +0530

    Complete A2 Part3 : Add Input Control to TopDownCharacter

--- Source/UnrealAssignments/TP_TopDown/TP_TopDownCharacter.cpp
+++ Source/UnrealAssignments/TP_TopDown/TP_TopDownCharacter.cpp
@@ -43,9 +43,100 @@ ATP_TopDownCharacter::ATP_TopDownCharacter()
 	// Activate ticking in order to update the cursor every frame.
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = true;
+
+	CachedDestination = FVector::ZeroVector;
+	FollowTime = 0.f;
 }
 
 void ATP_TopDownCharacter::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
+}
+
+void ATP_TopDownCharacter::BeginPlay() {
+	// Call the base class  
+	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void ATP_TopDownCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent)) {
+		// Setup mouse input events
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Started, this, &ATP_TopDownCharacter::OnInputStarted);
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Triggered, this, &ATP_TopDownCharacter::OnSetDestinationTriggered);
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Completed, this, &ATP_TopDownCharacter::OnSetDestinationReleased);
+		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Canceled, this, &ATP_TopDownCharacter::OnSetDestinationReleased);
+
+		// Setup touch input events
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Started, this, &ATP_TopDownCharacter::OnInputStarted);
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Triggered, this, &ATP_TopDownCharacter::OnTouchTriggered);
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Completed, this, &ATP_TopDownCharacter::OnTouchReleased);
+		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Canceled, this, &ATP_TopDownCharacter::OnTouchReleased);
+	}
+}
+
+void ATP_TopDownCharacter::OnInputStarted() {
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	PlayerController->StopMovement();
+}
+
+void ATP_TopDownCharacter::OnSetDestinationTriggered() {
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+
+	// We flag that the input is being pressed
+	FollowTime += GetWorld()->GetDeltaSeconds();
+
+	// We look for the location in the world where the player has pressed the input
+	FHitResult Hit;
+	bool bHitSuccessful = false;
+	if (bIsTouch) {
+		bHitSuccessful = PlayerController->GetHitResultUnderFinger(ETouchIndex::Touch1, ECollisionChannel::ECC_Visibility, true, Hit);
+	}
+	else {
+		bHitSuccessful = PlayerController->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit);
+	}
+
+	// If we hit a surface, cache the location
+	if (bHitSuccessful) {
+		CachedDestination = Hit.Location;
+	}
+
+	// Move towards mouse pointer or touch
+	APawn* ControlledPawn = PlayerController->GetPawn();
+	if (ControlledPawn != nullptr) {
+		FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
+		ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
+	}
+}
+
+void ATP_TopDownCharacter::OnSetDestinationReleased() {
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+
+	// If it was a short press
+	if (FollowTime <= ShortPressThreshold) {
+		// We move there and spawn some particles
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(PlayerController, CachedDestination);
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(PlayerController, FXCursor, CachedDestination, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
+	}
+
+	FollowTime = 0.f;
+}
+
+void ATP_TopDownCharacter::OnTouchTriggered() {
+	bIsTouch = true;
+	OnSetDestinationTriggered();
+}
+
+void ATP_TopDownCharacter::OnTouchReleased() {
+	bIsTouch = false;
+	OnSetDestinationReleased();
 }
--- Source/UnrealAssignments/TP_TopDown/TP_TopDownCharacter.h
+++ Source/UnrealAssignments/TP_TopDown/TP_TopDownCharacter.h
@@ -4,7 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/PlayerController.h"
+#include "Templates/SubclassOf.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "GameFramework/Pawn.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
 #include "TP_TopDownCharacter.generated.h"
+
+class UNiagaraSystem;
 
 UCLASS(Blueprintable)
 class ATP_TopDownCharacter : public ACharacter
@@ -30,5 +41,40 @@ private:
 	/** Camera boom positioning the camera above the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* CameraBoom;
+
+public:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	FVector CachedDestination;
+	uint32 bMoveToMouseCursor : 1;
+	bool bIsTouch; // Is it a touch device
+	float FollowTime; // For how long it has been pressed
+
+	/** Time Threshold to know if it was a short press */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	float ShortPressThreshold;
+
+	/** FX Class that we will spawn when clicking */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UNiagaraSystem* FXCursor;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* SetDestinationClickAction;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* SetDestinationTouchAction;
+
+	void OnInputStarted();
+	void OnSetDestinationTriggered();
+	void OnSetDestinationReleased();
+	void OnTouchTriggered();
+	void OnTouchReleased();
 };
 
