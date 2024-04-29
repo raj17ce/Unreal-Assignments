e308958082f91516a1eef62053e4d64fb82d807a
Raj Patel <raj17ce@gmail.com>
2024-04-29 17:29:22 +0530

    Add Plugin for Assignment-3

---
+++ Plugins/Assignment3/Source/Assignment3/Private/TopDownPawn.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TopDownPawn.h"
+
+// Sets default values
+ATopDownPawn::ATopDownPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATopDownPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATopDownPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATopDownPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
