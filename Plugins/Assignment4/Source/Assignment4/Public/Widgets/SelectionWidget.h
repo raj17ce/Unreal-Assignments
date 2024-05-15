283ee976cf5e7b63b169a8ec0dabef37d7e6bcac
Raj Patel <raj17ce@gmail.com>
2024-05-15 09:55:18 +0530

    A4 Part2 : Create SelectionWidget C++ Class and UMG Blueprint

---
+++ Plugins/Assignment4/Source/Assignment4/Public/Widgets/SelectionWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SelectionWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENT4_API USelectionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
