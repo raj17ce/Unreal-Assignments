1e5ca1fc623e61600d6acb81473c5cb1ca34b439
Raj Patel <raj17ce@gmail.com>
2024-06-05 18:45:07 +0530

    Create MeshDataAsset class and DA_MeshDataAsset

---
+++ Plugins/Assignment_5_6/Source/Assignment_5_6/Public/MeshDataAsset.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MeshDataAsset.generated.h"
+
+USTRUCT(Blueprintable)
+struct FStaticMeshData {
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinScale;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxScale;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator MinRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator MaxRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxDistance;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENT_5_6_API UMeshDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="MeshData")
+	TArray<FStaticMeshData> MeshesData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Materials")
+	TArray<UMaterial*> Materials;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Colours")
+	TArray<FLinearColor> Colours;
+};
